771b51f6e4df2469e74d2f89ff42bce10c306f98
oanaapostol <oana.apostol85@gmail.com>
2020-06-20 14:47:04 +0200

    Add gradle wrapper as build tool

---
+++ src/main/java/com/swissre/cryptocalculator/CryptoCalculatorApplication.java
@@ -0,0 +1,8 @@
+package com.swissre.cryptocalculator;
+
+public class CryptoCalculatorApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Crypto Calculator!");
+    }
+}
