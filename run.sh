d89e9765830c51d6f656629ba3671a33c3128e1f
oanaapostol <oana.apostol85@gmail.com>
2020-06-23 00:48:23 +0200

    Fix run commands for Windows

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 mkdir -p out
 javac -d out src/main/java/com/swissre/cryptocalculator/converter/*.java src/main/java/com/swissre/cryptocalculator/*.java
-javac -d out -cp out:lib/junit-platform-console-standalone-1.7.0-M1.jar src/test/java/com/swissre/cryptocalculator/converter/*.java src/test/java/com/swissre/cryptocalculator/*.java
-java -jar lib/junit-platform-console-standalone-1.7.0-M1.jar -cp out:src/test/resources --scan-classpath
 java -cp out com.swissre.cryptocalculator.CryptoWalletApplication
