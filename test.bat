7803e8a56de3dd52b37c7acbcece440b2d601855
oanaapostol <oana.apostol85@gmail.com>
2020-06-23 00:57:13 +0200

    Fix run commands for Windows

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
-mkdir -p out
+if not exist out mkdir out
 javac -d out src/main/java/com/swissre/cryptocalculator/converter/*.java src/main/java/com/swissre/cryptocalculator/*.java
 javac -d out -cp out;lib/junit-platform-console-standalone-1.7.0-M1.jar src/test/java/com/swissre/cryptocalculator/converter/*.java src/test/java/com/swissre/cryptocalculator/*.java
 java -jar lib/junit-platform-console-standalone-1.7.0-M1.jar -cp out;src/test/resources --scan-classpath
