35f657b757228095c2a4aa56f71732b1a2b604f8
oanaapostol <oana.apostol85@gmail.com>
2020-06-22 02:16:41 +0200

    Set currency to EUR as default.

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # Crypto Wallet Calculator
-Crypto currency to currency calculator.
+Crypto currency to EUR calculator.
 Application input: 
-- the path of the file containing cryptocurrencies amount
-- the conversion currency 
+- the path of the file containing crypto currencies amount
 
-## Getting Started
-- Run the following command in the project root:
-
-      ./gradlew run
-- Enter the path of the file containing crypto currencies amount when prompted 
-- Enter the currency in ISO format( e.g. EUR) when prompted 
-    
 ## Crypto Wallet File Content Example  
 
     BTC=10
     ETH=5
     XRP=2000   
+    
+## Getting Started
+- Run the following command in the project root:
 
+      ./gradlew run
+
+- Enter the path of the file containing crypto currencies amount when prompted 
+   
+
+## Run unit tests
+- Run the following command in the project root:
+
+      ./gradlew test  
--- src/main/java/com/swissre/cryptocalculator/CryptoWalletApplication.java
+++ src/main/java/com/swissre/cryptocalculator/CryptoWalletApplication.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class CryptoWalletApplication {
 
+    public static final String EUR = "EUR";
+
     public static void main(String[] args) {
         System.out.println("Welcome to Crypto Calculator!");
 
         Scanner scanner = new Scanner(System.in);
         String cryptoWalletFilePath = readCryptoWalletFilePath(scanner);
-        String currency = readCurrency(scanner);
 
         new CryptoWalletCalculatorBuilder()
                 .withCryptoWalletFilePath(cryptoWalletFilePath)
-                .withCurrency(currency)
+                .withCurrency(EUR)
                 .build()
                 .calculate()
                 .prettyPrint();
@@ -21,11 +22,6 @@ public class CryptoWalletApplication {
 
     private static String readCryptoWalletFilePath(Scanner scanner) {
         System.out.print("Enter the path of the crypto wallet file: ");
-        return scanner.next();
-    }
-
-    private static String readCurrency(Scanner scanner) {
-        System.out.print("Enter conversion currency (e.g. EUR): ");
         return scanner.next();
     }
 }
